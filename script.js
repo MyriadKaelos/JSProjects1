b69dd0ef4c0f2fe3ae62690f1f386371d78e6a45
Myriad Kaelos <yateslough@gmail.com>
2017-09-19 09:52:50 -0700

    Adding the formatting for 10 new problems. mod 2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /**
  * Created by h205p2 on 9/1/17.
  */
-//write first method
 function sleepIn(param1,param2) {
+    //write first method
     if(param2 == true) {
         return param2;
     } else if (param2 == false){
@@ -13,8 +13,8 @@ function sleepIn(param1,param2) {
         }
     }
 }
-//write second method
 function monkeyTrouble(param1,param2)  {
+    //write second method
     if(param1 == true) {
         if(param2 == true) {
             //both smiling
@@ -33,8 +33,8 @@ function monkeyTrouble(param1,param2)  {
         }
     }
 }
-//write third method
 function stringTimes(param1,param2) {
+    //write third method
     var final = "";
     for(var i = 0; i < param2; i++) {
         final += param1;
@@ -42,8 +42,8 @@ function stringTimes(param1,param2) {
     }
     return final;
 }
-//write fourth method
 function frontTimes(param1,param2) {
+    //write fourth method
     var final = "";
     var subParam = param1.substring(0,3);
     for(var i = 0; i < param2; i++) {
@@ -52,8 +52,8 @@ function frontTimes(param1,param2) {
     }
     return final;
 }
-//write fifth method
 function stringBits(param1) {
+    //write fifth method
     var STRING_LENGTH = param1.length;
     var final = "";
     for(var i = 0; i < STRING_LENGTH; i++) {
@@ -64,8 +64,8 @@ function stringBits(param1) {
     }
     return final;
 }
-//write sixth method
 function caughtSpeeding(param1,param2) {
+    //write sixth method
     var ticket = 0;
     var kindNess = 0;
     if(param2 == true) {
@@ -80,8 +80,8 @@ function caughtSpeeding(param1,param2) {
     }
     return ticket;
 }
-//write seventh method
 function fizzBuzz(param1) {
+    //write seventh method
     var final = "";
     if(param1 % 3 == 0) {
         final += "Fizz";
@@ -96,8 +96,8 @@ function fizzBuzz(param1) {
     }
     return final;
 }
-//write eighth method
 function teaParty(param1,param2) {
+    //write eighth method
     var party = 0;
     var great = false;
     if(param1/2 >= param2 || param2/2 >= param1) {
@@ -111,8 +111,8 @@ function teaParty(param1,param2) {
     }
     return party;
 }
-//write ninth method
 function blackJack(param1,param2) {
+    //write ninth method
     if(param1 >= param2 && param1 <=21) {
         return param1;
     } else if(param2 > param1 && param2 <=21) {
@@ -125,8 +125,8 @@ function blackJack(param1,param2) {
         return param2;
     }
 }
-//write tenth method
 function loneSum(param1,param2,param3) {
+    //write tenth method
     var sum = 0;
     if(param1 != param2 && param1 != param3) {
         sum += param1;
@@ -139,45 +139,142 @@ function loneSum(param1,param2,param3) {
     }
     return sum;
 }
-//write eleventh method
 function firstLast6(param1) {
-
+    //write eleventh method
+    var last = param1.length - 1;
+    var final = param1[last];
+    if(final == 6) {
+        return true;
+    } else if(param1[0] == 6) {
+        return true;
+    } else {
+        return false;
+    }
 }
-//write twelfth method
 function has_23(param1) {
-
+    //write twelfth method
+    if(param1[0] == 3 || param1[0] == 2 || param1[1] == 3 || param1[1] == 2) {
+        return true;
+    }
+    return false;
 }
-//write thirteenth method
 function fix_23(param1) {
-
+    //write thirteenth method
+    if(param1[0] == 2) {
+        if(param1[1] == 3) {
+            //change param1[1] to 0
+            param1[1] = 0;
+        } else if(param1[1] == 2) {
+            if(param1[2] == 3) {
+                //change param1[2] to 0
+                param1[2] = 0;
+            }
+        }
+    } else if(param1[1] == 2) {
+        if(param1[2] == 3) {
+            param1[2] = 0;
+        }
+    }
+    return param1;
 }
-//write fourteenth method
 function countYZ(param1) {
-
+//write fourteenth method
+    var count = param1.length;
+    var sum = 0;
+    for(var i = 0; i < count; i ++) {
+        var str = param1[i];
+        var len = str.length;
+        var sub = str.substring(len-1,len);
+        if(sub == "y" || sub == "z") {
+            sum += 1;
+        }
+    }
+    return sum;
 }
-//write fifteenth method
 function endOther(param1) {
-
+//write fifteenth method
+    var str1 = param1[0];
+    var str2 = param1[1];
+    var diffValue = Math.abs(str1.length - str2.length);
+    if(str1.length > str2.length) {
+        var str3 = str1;
+        var str4 = str2;
+    } else if (str1.length < str2.length) {
+        var str3 = str2;
+        var str4 = str1;
+    } else if (str1.length == str2.length) {
+        if(str1 == str2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    var sub = str3.substring(0+diffValue);
+    if(sub == str4) {
+        return true;
+    } else if (sub != str4) {
+        return false;
+    }
 }
-//write sixteenth method
 function starOut(param1) {
-
+//write sixteenth method
+    var str = param1;
+    while(true) {
+        var pos = str.indexOf("*");
+        if(pos < 0) {
+            break;
+        }
+        str = str.substring(0,pos-1) + str.substring(pos+2);
+    }
+    return str;
 }
-//write seventeenth method
 function getSandwich(param1) {
-
+//write seventeenth method
+    var pos = param1.indexOf("bread");
+    var str = param1.substring(pos+5);
+    pos = str.indexOf("bread");
+    str = str.substring(0,pos);
+    return str;
 }
-//write eighteenth method
 function canBalance(param1) {
-
+//write eighteenth method
+    var sum = 0;
+    var sumOther = 0;
+    for(var i = 0; i < param1.length; i++) {
+        sum += param1[i];
+    }
+    for(var i = 0; i < param1.length; i++) {
+        if(sumOther < sum/2) {
+            sumOther += param1[i]
+        }
+    }
+    if(sumOther == sum/2) {
+        return true;
+    } else {
+        return false;
+    }
 }
-//write nineteenth method
 function countClumps(param1) {
-
+//write nineteenth method
+    var count = 0;
+    var last = "nonInt";
+    for(var i = 0; i < param1.length; i++ ) {
+        if(param1[i] == last) {
+            count += 1;
+        }
+         last = param1[i];
+    }
+    return count;
 }
-//write twentieth method
 function evenlySpaced(param1) {
-
+//write twentieth method
+    var diff1 = param1[1] - param1[0];
+    var diff2 = param1[2] - param1[1];
+    if(diff1 == diff2) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function tester1() {
@@ -186,29 +283,29 @@ function tester1() {
     //return true if param2 is true, or param1 is false
     //return false if  both are false
     document.getElementById("output0").innerHTML = sleepIn(false, false);
-}
+}//sleepIn
 function tester2() {
     //output becomes the return of monkeyTrouble
     //monkeys smiling? bad idea
     //if one is then return false
     //if both or none return true
     document.getElementById("output1").innerHTML = monkeyTrouble(false, false);
-}
+}//monkeyTrouble
 function tester3() {
     //output becomes the return of stringTimes
     //returns a string n times
     document.getElementById("output2").innerHTML = stringTimes("Hey",2);
-}
+}//stringTimes
 function tester4() {
     //output becomes the return of frontTimes
     //returns the first three letters of the string n times
     document.getElementById("output3").innerHTML = frontTimes("Cool",4);
-}
+}//frontTimes
 function tester5() {
     //output becomes the return of stringBits
     //returns every other letter of the string
     document.getElementById("output4").innerHTML = stringBits("curse aisle");
-}
+}//stringBits
 function tester6() {
     //output becomes the return of caughtSpeeding
     //0 to 60 returns no ticket,(0)
@@ -216,7 +313,7 @@ function tester6() {
     //81 and up returns a big ticket,(2)
     //unless it's your birthday, then your speed can be five higher
     document.getElementById("output5").innerHTML = caughtSpeeding(83,true);
-}
+}//caughtSpeeding
 function tester7() {
     //output becomes the return of FizzBuzz
     //returns the string form of a number followed by !
@@ -224,7 +321,7 @@ function tester7() {
     //unless by five then return Buzz
     //if both 5 and 3 return FizzBuzz
     document.getElementById("output6").innerHTML = fizzBuzz(30);
-}
+}//fizzBuzz
 function tester8() {
     //output becomes the return of teaParty
     //returns 0 = bad party:
@@ -233,50 +330,50 @@ function tester8() {
     //one is more than twice the other
 
     document.getElementById("output7").innerHTML = teaParty(5,15);
-}
+}//teaParty
 function tester9() {
     //output becomes the return of blackJack
     //return one integer, of the two, whichever is closest to 21 without going over
     //returns 0 if they both go over
     document.getElementById("output8").innerHTML = blackJack(22,19);
-}
+}//blackJack
 function tester10() {
     //output becomes the return of loneSum
     //returns the sum of a, b, and c.
     //unless a value is the same as another
     //ie: 5,4,6 = 15; 3,3,3 = 0; 5,5,16 = 16
     document.getElementById("output9").innerHTML = loneSum(3,6,9);
-}
+}//loneSum
 function tester11() {
     //output becomes the return of firstLast6
         //arrays can be written by calling a
         //var y = ["c","a","l","l","m","e"];
     //firstLast6 gets an array of integers
     //if the first or last is "6" return true
-    document.getElementById("output10").innerHTML = firstLast6([4,5,2,3,7,6]);
-}
+    document.getElementById("output10").innerHTML = firstLast6([6,5,2,3,7,6,7,2]);
+}//firstLast6
 function tester12() {
     //output becomes the return of has_23
     //given an array of two integers,
     //return true if either is a two or three
-    document.getElementById("output11").innerHTML = has_23();
-}
+    document.getElementById("output11").innerHTML = has_23([3,1]);
+}//has_23
 function tester13() {
     //output becomes the return of fix_23
     //given an array of three integers,
     //if an integer 2 is followed by a three
     //return the array with the 3 turned into a zero
     //ie: 234 = 204
-    document.getElementById("output12").innerHTML = fix_23();
-}
+    document.getElementById("output12").innerHTML = fix_23([2,3,2]);
+}//fix_23
 function tester14() {
     //output becomes the return of countYZ
     //returns the amount of words ending in
     //y or z in a string of words
     //ie: fray fez = 2
     //ie: cold fez = 1
-    document.getElementById("output13").innerHTML = countYZ();
-}
+    document.getElementById("output13").innerHTML = countYZ(["fez","gourd","shaky","ally","matriarch"]);
+}//countYZ
 function tester15() {
     //output becomes the return of endOther
     //given two strings returns true if one
@@ -284,39 +381,39 @@ function tester15() {
     //ie: "abVow" and "crabVow" would return true
     //str.toLowerCase() returns the lowercase version
     //of string str
-    document.getElementById("output14").innerHTML = endOther();
-}
+    document.getElementById("output14").innerHTML = endOther(["got","forgot"]);
+}//endOther
 function tester16() {
     //output becomes the return of starOut
     //given a string, the return is the string, but
     //where there is a "*" character, the characters
     //to its right and left are gone
     //ie: "canni*bal" returns "cannal"
-    document.getElementById("output15").innerHTML = starOut();
-}
+    document.getElementById("output15").innerHTML = starOut("r*e*member");
+}//starOut
 function tester17() {
     //output becomes the return of getSandwich
     //given a string, returns only the party of the
     //string between two slices of bread
     //ie: "plasticbreadhoneybread" returns "honey"
-    document.getElementById("output16").innerHTML = getSandwich();
-}
+    document.getElementById("output16").innerHTML = getSandwich("boxbreadlettucehamcheesemayobreadbox");
+}//getSandwich
 function tester18() {
     //output becomes the return of canBalance
     //given an array of numbers, returns true
     //if the array can be split with equal
     //weight on either side
     //ie: ([5,2,1,8]) would return true
-    document.getElementById("output17").innerHTML = canBalance();
-}
+    document.getElementById("output17").innerHTML = canBalance([4,6,4,11,3,4,7,11]);
+}//canBalance
 function tester19() {
     //output becomes the return of countClumps
     //given an array, the return is the amount
     //of clumps in the array, clumps being
     //two adjacent same integers
     //ie: ([1,1,4,5,7,7,5]) returns 2
-    document.getElementById("output18").innerHTML = countClumps();
-}
+    document.getElementById("output18").innerHTML = countClumps([3,3,4,6,1,2,3,3,4,4,4,4,6,7]);
+}//countClumps
 function tester20() {
     //output becomes the return of evenlySpaced
     //given an array of three integers
@@ -324,5 +421,5 @@ function tester20() {
     //is evenly spaced
     //ie: ([6,5,4]) true
     //ie: ([1,3,4]) false
-    document.getElementById("output19").innerHTML = evenlySpaced();
-}
+    document.getElementById("output19").innerHTML = evenlySpaced([9,2,-5]);
+}//evenlySpaced
